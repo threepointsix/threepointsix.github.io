c56b6c8bf00dba274b5ad630ddad15b578c211e2
Akmal <akmalmuxiddinov@mail.ru>
2021-02-15 13:28:29 +0100

    Changes in code
    Deleted unnecessary .sass and jquery files
    Formatted code using prettier

--- Battleship/script/script.js
+++ Battleship/script/script.js
@@ -1,58 +1,60 @@
 import { enemy } from "./battleship.js";
 
 function enemyTable() {
-	let battleMaps = document.getElementById('battleMaps'),
-		tbl = document.createElement('table');
+  let battleMaps = document.getElementById("battleMaps"),
+    tbl = document.createElement("table");
 
-	tbl.style.margin = '0 auto';
+  tbl.style.margin = "0 auto";
 
-	let letterArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+  let letterArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
 
-	for (let i = 0; i < 10; i++) {
-		let tr = tbl.insertRow();
-		for (let j = 0; j < 10; j++) {
-			let td = tr.insertCell();
-			let block = td.appendChild(document.createElement("div"));
-			block.className = 'block';
-			block.style.width = '3vw';
-			block.style.height = block.style.width;
-			block.style.background = 'white';
-			block.style.outline = 'none';
-			block.style.margin = '0 0';
-			block.style.padding = '0 0';
-			block.style.border = '1px solid black';
-			block.id = enemy.arrIndex(letterArr[i].toString(), j + 1).toString();
-			block.addEventListener("click", function () { enemy.buttonClick(block); });
-		}
-	}
-	battleMaps.appendChild(tbl);
+  for (let i = 0; i < 10; i++) {
+    let tr = tbl.insertRow();
+    for (let j = 0; j < 10; j++) {
+      let td = tr.insertCell();
+      let block = td.appendChild(document.createElement("div"));
+      block.className = "block";
+      block.style.width = "3vw";
+      block.style.height = block.style.width;
+      block.style.background = "white";
+      block.style.outline = "none";
+      block.style.margin = "0 0";
+      block.style.padding = "0 0";
+      block.style.border = "1px solid black";
+      block.id = enemy.arrIndex(letterArr[i].toString(), j + 1).toString();
+      block.addEventListener("click", function () {
+        enemy.buttonClick(block);
+      });
+    }
+  }
+  battleMaps.appendChild(tbl);
 }
 
 function userTable() {
-	let battleMaps = document.getElementById('battleMaps'),
-		tbl = document.createElement('table');
+  let battleMaps = document.getElementById("battleMaps"),
+    tbl = document.createElement("table");
 
-	tbl.style.margin = '0 auto';
+  tbl.style.margin = "0 auto";
 
-	let letterArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+  let letterArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
 
-	for (let i = 0; i < 10; i++) {
-		let tr = tbl.insertRow();
-		for (let j = 0; j < 10; j++) {
-			let td = tr.insertCell();
-			let block = td.appendChild(document.createElement("div"));
-			block.className = 'block';
-			block.style.width = '30px';
-			block.style.height = '30px';
-			block.style.background = 'white';
-			block.style.outline = 'none';
-			block.style.margin = '0 0';
-			block.style.padding = '0 0';
-			block.style.border = '1px solid black';
-			block.id = arrIndex(letterArr[i].toString(), j + 1).toString();
-		}
-	}
-	battleMaps.appendChild(tbl);
+  for (let i = 0; i < 10; i++) {
+    let tr = tbl.insertRow();
+    for (let j = 0; j < 10; j++) {
+      let td = tr.insertCell();
+      let block = td.appendChild(document.createElement("div"));
+      block.className = "block";
+      block.style.width = "30px";
+      block.style.height = "30px";
+      block.style.background = "white";
+      block.style.outline = "none";
+      block.style.margin = "0 0";
+      block.style.padding = "0 0";
+      block.style.border = "1px solid black";
+      block.id = arrIndex(letterArr[i].toString(), j + 1).toString();
+    }
+  }
+  battleMaps.appendChild(tbl);
 }
 
 enemyTable();
--- js/script.js
+++ js/script.js
@@ -1,41 +1,29 @@
 function copyUsername() {
-    const textAr = document.createElement("textarea");
-    textAr.value = "@threepointsix";
-    textAr.setAttribute('readonly', '');
-    textAr.style.position = 'absolute';
-    textAr.style.left = '-9999px';
-    document.body.appendChild(textAr);
-    textAr.select();
-    document.execCommand("copy");
-    document.body.removeChild(textAr);
-    copied();
+  const textAr = document.createElement("textarea");
+  textAr.value = "@threepointsix";
+  textAr.setAttribute("readonly", "");
+  textAr.style.position = "absolute";
+  textAr.style.left = "-9999px";
+  document.body.appendChild(textAr);
+  textAr.select();
+  document.execCommand("copy");
+  document.body.removeChild(textAr);
+  copied();
 }
 
 function showTelegram() {
-    let tgCopy = document.getElementById("tg-copy");
-    (tgCopy.style.display === "none") ? (tgCopy.style.display = "block") : tgCopy.style.display = "none";
+  let tgCopy = document.getElementById("tg-copy");
+  tgCopy.style.display === "none"
+    ? (tgCopy.style.display = "block")
+    : (tgCopy.style.display = "none");
 }
 
 function hideCopied() {
-    let tgCopied = document.getElementById("tg-copied");
-    tgCopied.style.display = "none";
+  let tgCopied = document.getElementById("tg-copied");
+  tgCopied.style.display = "none";
 }
 
 function copied() {
-    let tgCopied = document.getElementById("tg-copied");
-    tgCopied.style.display = "block";
+  let tgCopied = document.getElementById("tg-copied");
+  tgCopied.style.display = "block";
 }
-
-// function battleshipButtonShow() {
-//     let battleship = document.getElementById("battleship");
-//     let battleshipImg = document.getElementById("battleshipImg");
-//     battleshipImg.style.filter = "brightness(30%)";
-//     battleship.style.visibility = "visible";
-// }
-
-// function battleshipButtonHide() {
-//     let battleship = document.getElementById("battleship");
-//     let battleshipImg = document.getElementById("battleshipImg");
-//     battleshipImg.style.filter = "brightness(100%)";
-//     battleship.style.visibility = "hidden";
-// }
